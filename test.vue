1488411cf739efd26b88ba84d9d19cabbca1074c
TTiip <2711430287@qq.com>
2022-04-19 16:35:01 +0800

    feat: vue格式化相关配置

---
+++ test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>123</div>
+</template>
+
+<script lang='ts' setup>
+  import { ref } from 'vue'
+
+  switch (a) {
+  case 'a':
+    break
+  case 'b':
+    break
+  }
+
+</script>
+
+<style lang='less'></style>
