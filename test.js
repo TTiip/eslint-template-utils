062b26ccfdeec92f944c9063989a68b177af60b4
TTiip <2711430287@qq.com>
2022-04-19 16:34:50 +0800

    feat: js格式化相关配置

---
+++ test.js
@@ -0,0 +1,19 @@
+const a1 = (test) => {
+  debugger
+  console.log(test)
+}
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const obj1 = {
+  a: 123,
+  b: 234,
+  fcuntion(aa, bb) {
+    console.log(aa)
+    console.log(bb)
+  }
+}
+
+console.log(a1)
+console.log(arr1)
+console.log(obj1)
